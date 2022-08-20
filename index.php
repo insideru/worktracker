edcba9a1f5d3e9d63b21fc3da34c200bd8097578
Insideru <insideru@gmail.com>
2022-08-21 02:45:41 +0300

    v 0.1.4

--- index.php
+++ index.php
@@ -7,37 +7,35 @@ $account->sessionLogin();
 if ($account->authenticated) {
     //user e logat
     if (isset($_GET['page'])) {
-        if ($_GET['page']=="dashboard"){
+        if ($_GET['page'] == "dashboard") {
             require 'proto-dashboard.html';
             die;
-        } else if ($_GET['page']=="calendar"){
+        } else if ($_GET['page'] == "calendar") {
             require 'proto-calendar.html';
             die;
-        } else if ($_GET['page']=="ponteaza"){
+        } else if ($_GET['page'] == "ponteaza") {
             require 'proto-pontaj.html';
             die;
-        } else if ($_GET['page']=="concediu"){
+        } else if ($_GET['page'] == "concediu") {
             require 'proto-concediu.html';
             die;
-        } else if ($_GET['page']=="admin"){
+        } else if ($_GET['page'] == "admin") {
             require "proto-admin.html";
             die;
-        } else if ($_GET['page']=="newProject"){
+        } else if ($_GET['page'] == "newProject") {
             require "proto-newproj.html";
             die;
-        } else if ($_GET['page']=="reports"){
+        } else if ($_GET['page'] == "reports") {
             require "proto-raport.html";
             die;
-        } else if ($_GET['page']=="logout"){
+        } else if ($_GET['page'] == "logout") {
             logout();
             header("Location: /");
             die;
         }
     }
     require 'proto-dashboard.html';
-    }
-    else {
-        //echo "Nu exista user logat";
-        require 'login.html';
-    }
-?>
+} else {
+    //echo "Nu exista user logat";
+    require 'proto-login.html';
+}
